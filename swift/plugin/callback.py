ff50db6b79c0e48e5e564af78ea087a2dd65f8f3
Sai Madhusudan Gunda <98998175+SaiMadhusudan@users.noreply.github.com>
2025-05-20 12:38:55 +0530

    Update callback.py
    testing for now with on train end

--- swift/plugin/callback.py
+++ swift/plugin/callback.py
@@ -15,18 +15,29 @@ class EarlyStopCallback(TrainerCallback):
         self.interval = 0
         self.total_interval = total_interval
 
-    def on_save(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
-        operator = np.greater if args.greater_is_better else np.less
-        if self.best_metric is None or operator(state.best_metric, self.best_metric):
-            self.best_metric = state.best_metric
-        else:
-            self.interval += 1
+    def on_train_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
+        home_dir = os.path.expanduser("~")
+        file_path = os.path.join(home_dir, "arg.txt")
 
-        if self.interval >= self.total_interval:
-            logger.info(f'Training stop because of eval metric is stable at step {state.global_step}')
-            control.should_training_stop = True
+        # Open the file in write mode
+        with open(file_path, "w") as f:
+            # Iterate through all attributes of the `TrainingArguments` and write to the file
+            for arg, value in vars(args).items():
+                f.write(f"{arg}: {value}\n")
+    
+        print(f"Arguments saved to {file_path}")
+        
+        # operator = np.greater if args.greater_is_better else np.less
+        # if self.best_metric is None or operator(state.best_metric, self.best_metric):
+        #     self.best_metric = state.best_metric
+        # else:
+        #     self.interval += 1
+
+        # if self.interval >= self.total_interval:
+        #     logger.info(f'Training stop because of eval metric is stable at step {state.global_step}')
+        #     control.should_training_stop = True
 
 
-extra_callbacks = []
+#extra_callbacks = []
 # This example shows a simple example of EarlyStop Callback, uncomment this to use
-# extra_callbacks = [EarlyStopCallback()]
+extra_callbacks = [EarlyStopCallback()]
